cdc5881f8af1608e6e9cc35bd7e58a73fba34eae
donrestarone <shashikejayatunge@gmail.com>
2018-05-15 15:46:09 -0400

    made first request

--- ajax.js
+++ ajax.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
-
-  /* Your code goes here */
-
+	var sectionOne = document.getElementById("step12");
+	var firstTitle = document.getElementById("titleOne");
+	$.ajax({
+		url: 'http://first-ajax-api.herokuapp.com/',
+		method: 'GET',
+	}).done(function(data){
+		console.log('made request')
+		firstTitle.innerText = 'made request'
+		console.log(data);
+	}).fail(function(){
+		console.log('failed')
+		firstTitle.innerText = 'Request failed'
+	})
 });
